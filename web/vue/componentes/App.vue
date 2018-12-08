ef9468d5a3d5e81a8ccdc7e76f4c3bd082af95bb
Yeison Lucio <yeisonlucio@gmail.com>
2018-12-08 18:31:32 -0500

    add vuejs

---
+++ web/vue/componentes/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            title: 'hola mundo'
+        }
+    }
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
-const webpack = require('webpack');
+const {VueLoaderPlugin} = require('vue-loader');
+module.exports = {
+    mode: 'development',
+    entry: './web/vue/index.js',
+    output: {
+        path: __dirname + '/web/dist',
+        filename: 'bundle.js'
+    },
 
-module.export = {
-  entry: './web/js/script.js',
-  output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
-  }
+    devServer: {
+        port: 8000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                      }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+            
+
+
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+    
+    
+
 }
